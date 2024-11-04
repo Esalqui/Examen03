259e18fd7c9686db845790d1468d165bd4227d43
elmerSQ <elmer@ASUS-EGSQ>
2024-11-04 17:31:17 -0500

    Agregar archivos de proyecto.

---
+++ Demo/Models/ProductoModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.Models
+{
+    public class ProductoModel
+    {
+        public string Nombres { get; set; }
+        public Double Precio { get; set; }   
+
+        public string Descripcion { get; set; }
+        public string UsuarioCreacion { get; set; }
+
+    }
+}
---
+++ Demo/ViewModels/ProductoViewModel.cs
@@ -0,0 +1,90 @@
+﻿using Demo.Models;
+using Demo.Utilitarios;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Demo.ViewModels
+{
+    public class ProductoViewModel : ViewModelBase
+    {
+        #region Propiedades
+
+        private string _Resultado;
+        public string Resultado
+        {
+            get { return _Resultado; }
+            set
+            {
+                _Resultado = value;
+                OnPropertyChanged(nameof(Resultado));
+            }
+        }
+
+
+        private string _Nombre;
+        public string Nombre
+        {
+            get { return _Nombre; }
+            set
+            {
+                _Nombre = value;
+                OnPropertyChanged(nameof(Nombre));
+            }
+        }
+
+        private double _Precio;
+        public double Precio
+        {
+            get { return _Precio; }
+            set
+            {
+                _Precio = value;
+                OnPropertyChanged(nameof(Precio));
+            }
+        }
+
+        private string _Descripcion;
+        public string Descripcion
+        {
+            get { return _Descripcion; }
+            set
+            {
+                _Descripcion = value;
+                OnPropertyChanged(nameof(Descripcion));
+            }
+        }
+
+        #endregion
+
+        public ObservableCollection<ProductoModel> Productos { get; }
+
+        #region Comando
+        public ICommand GuardarCommand { get; }
+
+        public ProductoViewModel()
+        {
+            GuardarCommand = new RelayCommand(Guardar);
+            Productos = new ObservableCollection<ProductoModel>();
+        }
+        private void Guardar()
+        {
+            Resultado = string.Concat(Nombre, " ", Precio, " ", Descripcion);
+
+            Productos.Add(new ProductoModel
+            {
+                Nombres = this.Nombre,
+                Precio = this.Precio,
+                Descripcion = this.Descripcion,
+                 UsuarioCreacion="htorrico"
+            });
+
+        }
+        #endregion
+
+    }
+}
